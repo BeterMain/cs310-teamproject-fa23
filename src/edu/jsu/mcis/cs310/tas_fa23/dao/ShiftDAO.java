4dec5387a6c81f6c1a1223d19ff547ad9be72209
Connor <cmunch10@protonmail.com>
2023-10-18 18:00:09 -0500

    Shift is now working

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/ShiftDAO.java
@@ -3,6 +3,8 @@ package edu.jsu.mcis.cs310.tas_fa23.dao;
 import java.sql.*;
 import edu.jsu.mcis.cs310.tas_fa23.Shift;
 import java.time.LocalTime;
+import java.util.HashMap;
+import edu.jsu.mcis.cs310.tas_fa23.Badge;
 
 /**
  *
@@ -11,11 +13,13 @@ import java.time.LocalTime;
 
 public class ShiftDAO {
     private final String idQuery = "select * from shift where id = ?";
-    private final String badgeQuery = "select * from shift where badge = ?";
+    private final String badgeQuery = "select * from employee where badgeid = ?";
     
     private final DAOFactory daoFactory;
     
-    LocalTime shiftStart = null, shiftStop = null, lunchStart = null, lunchStop = null;
+    private LocalTime shiftStart = null, shiftStop = null, lunchStart = null, lunchStop = null;
+    
+    private int shiftID = 0;
     
     ShiftDAO(DAOFactory daoFactory)
     {
@@ -47,23 +51,121 @@ public class ShiftDAO {
                     
                     if (rs.next())
                     {
-                        String description = rs.getString("description");
-                        String shiftStartString = rs.getString("shiftstart"); 
-                        String shiftStopString = rs.getString("shiftstop");
-                        int roundInterval = rs.getInt("roundinterval");
-                        int gracePeriod = rs.getInt("graceperiod");
-                        int dockPenalty = rs.getInt("dockpenalty");
-                        String lunchStartString = rs.getString("lunchstart");
-                        String lunchStopString  = rs.getString("lunchstop");
-                        int lunchThreshold = rs.getInt("lunchthreshold");
+                        HashMap<String, Object> hashmap = new HashMap<>();
+
                         
-                        shiftStart = LocalTime.parse(shiftStartString);
-                        shiftStop = LocalTime.parse(shiftStopString);
-                        lunchStart = LocalTime.parse(lunchStartString);
-                        lunchStop = LocalTime.parse(lunchStopString);
+                        hashmap.put("id", id);
+                        hashmap.put("description", rs.getString("description"));
+                        hashmap.put("shiftstart", rs.getTime("shiftstart"));
+                        hashmap.put("shiftstop", rs.getTime("shiftstop"));
+                        hashmap.put("roundinterval", rs.getInt("roundinterval"));
+                        hashmap.put("graceperiod",rs.getInt("graceperiod"));
+                        hashmap.put("dockpenalty",rs.getInt("dockpenalty"));
+                        hashmap.put("lunchstart", rs.getTime("lunchstart"));
+                        hashmap.put("lunchstop", rs.getTime("lunchstop"));
+                        hashmap.put("lunchthreshold",rs.getInt("lunchthreshold"));
+                        shift = new Shift(hashmap);
+                    }
+                }
+            }
+        }
+        catch(SQLException e) 
+        {
+            throw new DAOException(e.getMessage());   
+        }
+        
+        finally
+        {
+            if (rs != null)
+            {
+                try
+                {
+                    rs.close();
+                }
+                catch (SQLException e)
+                {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null)
+            {
+                try
+                {
+                    ps.close();
+                }
+                catch (SQLException e)
+                {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+        }
+        return shift;
+    }
+    
+    public Shift find(Badge badgeid)
+    {
+        Shift shift = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try
+        {
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0))
+            {
+                ps = conn.prepareStatement(badgeQuery);
+                ps.setString(1, badgeid.getId());
+                
+                boolean result = ps.execute();
+                
+                if (result)
+                {
+                    rs = ps.getResultSet();
+                    
+                    if (rs.next())
+                    {
+                        try 
+                        {
+                            shiftID = rs.getInt("shiftid");
+                        }
+                        catch (DAOException e)
+                        {
+                            throw new DAOException (e.getMessage());
+                        }
+                        finally
+                        {
+                            rs.close();
+                        }
+                    }
+                }
+                
+                ps = conn.prepareStatement(idQuery);
+                ps.setInt(1, shiftID);
+                
+                result = ps.execute();
+                
+                if(result)
+                {
+                    rs = ps.getResultSet();
+                    
+                    if (rs.next())
+                    {
+                        HashMap<String, Object> hashmap = new HashMap<>();
+
                         
-                        
-                        shift = new Shift(id, description, shiftStart, shiftStop, roundInterval, gracePeriod, dockPenalty, lunchStart, lunchStop, lunchThreshold);
+                        hashmap.put("id", shiftID);
+                        hashmap.put("description", rs.getString("description"));
+                        hashmap.put("shiftstart", rs.getTime("shiftstart"));
+                        hashmap.put("shiftstop", rs.getTime("shiftstop"));
+                        hashmap.put("roundinterval", rs.getInt("roundinterval"));
+                        hashmap.put("graceperiod",rs.getInt("graceperiod"));
+                        hashmap.put("dockpenalty",rs.getInt("dockpenalty"));
+                        hashmap.put("lunchstart", rs.getTime("lunchstart"));
+                        hashmap.put("lunchstop", rs.getTime("lunchstop"));
+                        hashmap.put("lunchthreshold",rs.getInt("lunchthreshold"));
+                        shift = new Shift(hashmap);
                     }
                 }
             }
