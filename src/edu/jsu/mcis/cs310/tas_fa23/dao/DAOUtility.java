75e8d663f2abdd99b1a97f2397f06d4502d6a1af
Connor <cmunch10@protonmail.com>
2023-10-25 12:13:35 -0500

    Merge branch 'master' of https://github.com/jtarver1/cs310-teamproject-fa23

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
@@ -5,7 +5,9 @@ import java.util.*;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import com.github.cliftonlabs.json_simple.*;
+import edu.jsu.mcis.cs310.tas_fa23.EventType;
 import edu.jsu.mcis.cs310.tas_fa23.Punch;
+import edu.jsu.mcis.cs310.tas_fa23.Shift;
 
 /**
  * 
@@ -43,5 +45,66 @@ public final class DAOUtility {
         result = Jsoner.serialize(jsonData);
         
         return result;
+    }
+    
+    public static int calculateTotalMinutes(ArrayList<Punch> dailyPunchList, Shift shift)
+    {
+        DAOFactory daoFactory = new DAOFactory("tas.jdbc");
+        PunchDAO punchDao = daoFactory.getPunchDAO();
+        ShiftDAO shiftDao = daoFactory.getShiftDAO();
+        
+        Boolean timeOut = false;
+        
+        Integer minutesWorked = 0;
+        
+        ArrayList<Punch> punchlist = dailyPunchList;
+        
+        for (Punch punch : punchlist)
+        {
+            punch.adjust(shift);
+            
+            if (punch.getPunchType() == EventType.TIME_OUT)
+            {
+                timeOut = true;
+            }
+        }
+        
+        if (punchlist.size() == 4)
+        {
+            LocalDateTime clockIn = (punchlist.get(0)).getAdjustedtimestamp();
+            LocalDateTime clockOut = (punchlist.get(3)).getAdjustedtimestamp();
+            LocalDateTime lunchStart = (punchlist.get(1)).getAdjustedtimestamp();
+            LocalDateTime lunchStop = (punchlist.get(2)).getAdjustedtimestamp();
+
+            Integer clockOutMinutes = (clockOut.getHour() * 60) + clockOut.getMinute();
+            Integer clockInMinutes = (clockIn.getHour() * 60) + clockIn.getMinute();
+            Integer lunchStopMinutes = (lunchStop.getHour() * 60) + lunchStop.getMinute();
+            Integer lunchStartMinutes = (lunchStart.getHour() * 60) + lunchStart.getMinute();
+            
+            minutesWorked = ((clockOutMinutes - clockInMinutes) - (lunchStopMinutes - lunchStartMinutes));
+        }
+        
+        else if (punchlist.size() == 2)
+        {
+            LocalDateTime clockIn  = punchlist.get(0).getAdjustedtimestamp();
+            LocalDateTime clockOut = punchlist.get(1).getAdjustedtimestamp();
+
+            Integer clockOutMinutes = (clockOut.getHour() * 60) + clockOut.getMinute();
+            Integer clockInMinutes = (clockIn.getHour() * 60) + clockIn.getMinute();
+
+            minutesWorked = (clockOutMinutes - clockInMinutes);
+            
+            if (minutesWorked > shift.getLunchThreshold())
+            {
+                minutesWorked -= 30;
+            }
+        }
+        
+        if (timeOut == true)
+        {
+            minutesWorked = 0;
+        }
+        
+        return minutesWorked;
     }
 }
