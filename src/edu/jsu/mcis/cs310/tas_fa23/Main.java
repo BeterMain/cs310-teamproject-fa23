d786d9b853db25a5e53f7d65ed34a033899d129d
Justin Toliver <jtoliver@stu.jsu.edu>
2023-12-01 10:37:36 -0600

    Adding modified Main.java

--- src/edu/jsu/mcis/cs310/tas_fa23/Main.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/Main.java
@@ -19,13 +19,13 @@ public class Main {
         BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
         
         // find badge
-
+        
         Badge b = badgeDAO.find("31A25435");
         
         // output should be "Test Badge: #31A25435 (Munday, Paul J)"
         
         System.err.println("Test Badge: " + b.toString());
         
+        }
+        
     }
-            
-}
